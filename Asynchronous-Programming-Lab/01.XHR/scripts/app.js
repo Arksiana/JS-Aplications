3b1cd3bfc10aaeed006d8b63820166c7dd3ea590
Arksiana <74001470+Arksiana@users.noreply.github.com>
2022-01-17 18:00:41 +0200

    upd

--- Asynchronous-Programming-Lab/01.XHR/scripts/app.js
+++ Asynchronous-Programming-Lab/01.XHR/scripts/app.js
@@ -1,4 +1,31 @@
 function loadRepos() {
-   console.log("TODO...");
-   fetch('')
+   let button = document.querySelector('#load');
+   button.addEventListener('click', function loadRepos() {
+      let url = 'https://api.github.com/users/testnakov/repos';
+      const httpRequest = new XMLHttpRequest();
+      httpRequest.addEventListener('readystatechange', function () {
+         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            document.getElementById('res').textContent = httpRequest.responseText;
+         }
+      });
+      httpRequest.open('GET', url);
+      httpRequest.send();
+   });
+
+}
+
+// way without adding the id load
+
+function loadRepos() {
+   const url = 'https://api.github.com/users/testnakov/repos';
+
+   const httpRequest = new XMLHttpRequest();
+   httpRequest.addEventListener('readystatechange', () => {
+      if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+         document.getElementById('res').textContent = httpRequest.responseText;
+      }
+   });
+
+   httpRequest.open('GET', url);
+   httpRequest.send();
 }
