b2acd371e8147a9925aafba8a98073241aa69420
Arksiana <74001470+Arksiana@users.noreply.github.com>
2022-01-17 18:37:14 +0200

    upd

--- Asynchronous-Programming-Lab/03.Gighub-Commits/app.js
+++ Asynchronous-Programming-Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,37 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
-}
+    const username = document.getElementById("username").value
+    const repo = document.getElementById("repo").value
+    const list = document.getElementById("commits");
+
+
+    const url = `https://api.github.com/repos/${username}/${repo}/commits`
+    
+        fetch(url)
+        .then((res) => {
+            if (res.ok == false) {
+                throw new Error(`${res.status} ${res.statusText}`);
+              }
+        
+              return res.json();
+            })
+        .then(handleResponse)
+        .catch(handleError)
+
+        function handleResponse(data) {
+            list.innerHTML = ''
+
+            for(let com of data){
+                const liElement = document.createElement('li')
+                liElement.innerHTML = `${com.commit.author.name}: ${com.commit.message}`;
+                list.appendChild(liElement)
+            
+            }
+        }
+
+
+        function handleError(err) {
+            list.innerHTML = ''
+            list.textContent = `${err.message}`
+          }
+        
+        }
