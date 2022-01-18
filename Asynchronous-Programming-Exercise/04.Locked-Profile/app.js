abce99412be10646eed7811e2aaf9d86ebe74ffd
Arksiana <74001470+Arksiana@users.noreply.github.com>
2022-01-18 22:29:32 +0200

    upd1

--- Asynchronous-Programming-Exercise/04.Locked-Profile/app.js
+++ Asynchronous-Programming-Exercise/04.Locked-Profile/app.js
@@ -1,31 +1,48 @@
-async function lockedProfile() {
-    const main = document.querySelector('main')
-    const url = `http://localhost:3030/jsonstore/advanced/profiles`
-    const res = await fetch(url)
-    const data = await res.json()
-    Object.values(data).forEach(profile => {
-        // main.replaceChildren()
-        const element = document.createElement('div')
-        element.className = 'profile'
-        element.innerHTML =`<img src="./iconProfile2.png" class="userIcon" />
+function profileTemplate({ username, email, age }, id) {
+    const wrapper = document.createElement('div')
+    const btn = document.createElement('button')
+    btn.innerText = 'Show more'
+
+    wrapper.className = 'profile'
+    wrapper.innerHTML = `<img src="./iconProfile2.png" class="userIcon">
 <label>Lock</label>
-<input type="radio" name="user1Locked" value="lock" checked>
+<input type="radio" name="user${id}Locked" value="lock" checked="">
 <label>Unlock</label>
-<input type="radio" name="user1Locked" value="unlock"><br>
+<input type="radio" name="user${id}Locked" value="unlock"><br>
 <hr>
 <label>Username</label>
-<input type="text" name="user1Username" value="" disabled readonly />
-<div class="hiddenInfo">
-    <hr>
-    <label>Email:</label>
-    <input type="email" name="user1Email" value="" disabled readonly />
-    <label>Age:</label>
-    <input type="email" name="user1Age" value="" disabled readonly />
-</div>
+<input type="text" name="user${id}Username" value=${username} disabled="" readonly="">
+<div id="user${id}HiddenFields">
+<hr>
+<label>Email:</label>
+<input type="email" name="user${id}Email" value=${email} disabled="" readonly="">
+<label>Age:</label>
+<input type="email" name="user${id}Age" value=${age} disabled="" readonly="">
+</div>`
 
-<button>Show more</button>`
-
-
-        main.appendChild(element)
+    btn.addEventListener('click', () => {
+        const checked = wrapper.querySelector('input[type=radio]:checked')
+        if (checked && checked.value === 'unlock') {
+            if (btn.innerText === 'Show more') {
+                wrapper.querySelector(`#user${id}HiddenFields`).style.display = 'block'
+                btn.innerText = 'Hide it'
+            } else {
+                wrapper.querySelector(`#user${id}HiddenFields`).style.display = 'none'
+                btn.innerText = 'Show more'
+            }
+        }
     })
+    wrapper.appendChild(btn)
+
+    return wrapper
+}
+
+async function lockedProfile() {
+    const data = await fetch(`http://localhost:3030/jsonstore/advanced/profiles`)
+    const des = await data.json()
+
+    const main = document.querySelector('main')
+    main.innerHTML = ''
+
+    Object.values(des).forEach((x, i) => main.appendChild(profileTemplate(x, i + 1)))
 }
